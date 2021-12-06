07fabc9ff66abcdacbaf6e599f89ce22846f9187
Loay Elshall <Loay.elshall@icloud.com>
2021-12-06 14:46:46 +0200

    linting fixes

--- distributed_store/src/App.vue
+++ distributed_store/src/App.vue
@@ -1,29 +1,52 @@
 <template>
   <div id="wrapper">
-    <nav class="navbar is-dark">
+    <nav class="navbar" role="navigation" aria-label="main navigation">
       <div class="navbar-brand">
-        <router-link to="/" class="navbar-item">eCommerce</router-link>
+        <a class="navbar-item" href="/">
+          <strong>eCommerce</strong>
+        </a>
+
         <a
+          role="button"
           class="navbar-burger"
           aria-label="menu"
           aria-expanded="false"
-          data-target="navbar-menu"
+          data-target="navbarBasicExample"
         >
-          <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
         </a>
       </div>
-      <div class="navbar-menu" id="navbar-menu">
+
+      <div id="navbarBasicExample" class="navbar-menu">
+        <div class="navbar-start">
+          <a class="navbar-item"> Documentation </a>
+
+          <div class="navbar-item has-dropdown is-hoverable">
+            <a class="navbar-link"> More </a>
+
+            <div class="navbar-dropdown">
+              <a class="navbar-item"> About </a>
+              <a class="navbar-item"> Jobs </a>
+              <a class="navbar-item"> Contact </a>
+              <hr class="navbar-divider" />
+              <a class="navbar-item"> Report an issue </a>
+            </div>
+          </div>
+        </div>
+
         <div class="navbar-end">
-          <router-link to="/register" class="navbar-item">
-            <button class="button is-link">Register</button>
-          </router-link>
-          <router-link to="/login" class="navbar-item"> Login </router-link>
+          <div class="navbar-item">
+            <div class="buttons">
+              <a class="button is-primary" href="/register">
+                <strong>Register</strong>
+              </a>
+              <a class="button is-light" href="/login">Login</a>
+            </div>
+          </div>
         </div>
       </div>
     </nav>
-
     <section class="section">
       <router-view />
     </section>
