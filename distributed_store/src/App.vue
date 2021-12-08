4b15de204414d9798ce96d372011d2ac4b549b0e
AmrAhmed11 <57914179+AmrAhmed11@users.noreply.github.com>
2021-12-08 20:54:06 +0200

    search BUGED

--- distributed_store/src/App.vue
+++ distributed_store/src/App.vue
@@ -12,11 +12,36 @@
           aria-label="menu"
           aria-expanded="false"
           data-target="navbarBasicExample"
+          @click="showMobileMenu = !showMobileMenu"
         >
+          <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
         </a>
       </div>
+
+      <div class="navbar-menu" id="navbar-menu" v-bind:class="{'is-active': showMobileMenu }">
+        <div class="navbar-start">
+          <div class="navbar-item">
+            <form method="get" action="/search">
+              <div class="field has-addons">
+                <div class="control">
+                  <input type="text" class="input" placeholder="What are you looking for?" name="query">
+                </div>
+                <div class="control">
+                  <button class="button is-success">
+                      <span class="icon">
+                      <i class="fas fa-search"></i>
+                      </span>
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+
+
 
       <div id="navbarBasicExample" class="navbar-menu">
         <div class="navbar-start">
--- distributed_store/src/router/index.js
+++ distributed_store/src/router/index.js
@@ -69,6 +69,9 @@ const routes = [
     path: "/:category_slug",
     name: "Category",
     component: Category,
+    meta: {
+      requireLogin: true,
+    }, 
   },
 ];
 
